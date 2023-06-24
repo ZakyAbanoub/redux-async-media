a8dc8e1bc66a4d1e53a433e5602f60c4d4576d70
Abanoub Zaky <zaky.abanoub@gmail.com>
2023-06-24 21:44:56 +0200

    initialize app and add usersSlice

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
+
+const UsersList = () => {
+  const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    // {data: [], isLoading : false, errore: null}
+    return state.users;
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Skeleton times={6} />;
+  }
+
+  if (error) {
+    return <div>Error fetching data..</div>;
+  }
+
+  return <div>{data.length}</div>;
+};
+
+export default UsersList;
