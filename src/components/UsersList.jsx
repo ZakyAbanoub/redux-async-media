274bc4a36565efec62cbac9ad8958ff3cd5c6734
Abanoub Zaky <zaky.abanoub@gmail.com>
2023-08-18 00:29:32 +0300

    add new feature add-users

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
+import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 const UsersList = () => {
   const dispatch = useDispatch();
   const { isLoading, data, error } = useSelector((state) => {
-    // {data: [], isLoading : false, errore: null}
     return state.users;
   });
 
@@ -14,15 +14,37 @@ const UsersList = () => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const addUserHandler = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) {
-    return <Skeleton times={6} />;
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
     return <div>Error fetching data..</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded ">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={addUserHandler}>Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UsersList;
--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,27 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchUsers } from "../thunks/fetchUsers";
+import { addUser } from "../thunks/addUser";
 
 const usersSlice = createSlice({
-  name: "users",
-  initialState: {
-    data: [],
-    isLoading: false,
-    error: null,
-  },
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(fetchUsers.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.data = action.payload;
-    });
-    builder.addCase(fetchUsers.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.error;
-    });
-  },
+    name: "users",
+    initialState: {
+        data: [],
+        isLoading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers(builder) {
+        //Fetch User
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+
+        //Add User
+        builder.addCase(addUser.pending, (state, action) => {
+            state.isLoading = true;
+        });
+        builder.addCase(addUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data.push(action.payload);
+        });
+        builder.addCase(addUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+    },
 });
 
 // export default usersSlice.reducer;
